73370c55f6825014ab79d35be6da377dbe12374e
MatthewFoster02 <mattfoster02@outlook.com>
2023-02-02 05:29:31 +0100

    styling complete

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-const options = {
-	method: 'GET',
-    mode: 'no-cors'
-};
+//toggle for dark mode
+const body = document.body;
+const toggleSwitch = document.getElementById("toggle");
 
-fetch('https://guarded-badlands-73293.herokuapp.com/news').then(res => res.json()).then(data => 
+toggleSwitch.addEventListener('change', (e) => 
 {
-    console.log(data)
-})
-.catch(err => console.error(err));
+    if(e.target.checked)
+    {
+        body.classList.remove("themeDark");
+        body.classList.add("themeLight");
+    }
+    else
+    {
+        body.classList.remove("themeLight");
+        body.classList.add("themeDark");
+    }
+});
+
+
+
+//First a function which is to be called every minute
+let refresh_content = function refreshContent()
+{
+    let rawData = [];
+    //First call to API
+    fetch('https://guarded-badlands-73293.herokuapp.com/news').then(res => res.json()).then(data => 
+    {
+        rawData = data;
+    })
+    .catch(err => console.error(err));
+
+    //Display on page
+    displayData(rawData);
+}
+
+function displayData(data)
+{
+
+}
+
+setInterval(refresh_content, 1000*60);
