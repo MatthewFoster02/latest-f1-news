ede297df559404100a5cad6116be80aa378c7203
MatthewFoster02 <mattfoster02@outlook.com>
2023-02-02 06:28:26 +0100

    added dynamic cards

--- main.js
+++ main.js
@@ -19,23 +19,67 @@ toggleSwitch.addEventListener('change', (e) =>
 
 
 //First a function which is to be called every minute
-let refresh_content = function refreshContent()
+//let refresh_content = 
+function refreshContent()
 {
     let rawData = [];
     //First call to API
     fetch('https://guarded-badlands-73293.herokuapp.com/news').then(res => res.json()).then(data => 
     {
-        rawData = data;
+        //Display on page
+        displayData(data);
     })
     .catch(err => console.error(err));
-
-    //Display on page
-    displayData(rawData);
 }
+refreshContent();
 
 function displayData(data)
 {
+    // Get parent div for cards
+    let parent = document.getElementById("parent");
+    // Loop through each article and create card object
+    data.forEach(article =>
+    {
+        // Create required elements
+        let card = document.createElement("a");
+        let logo = document.createElement("img");
+        let title = document.createElement('span');
 
+        // Set class of elements
+        card.setAttribute('class', 'card');
+        logo.setAttribute('class', 'source-logo');
+        title.setAttribute('class', 'article-title');
+
+        // Set article link and title
+        card.setAttribute('href', article.url);
+        card.setAttribute('target', '_blank');
+        title.innerHTML = article.title;
+
+        // Set correct logo
+        switch(article.source)
+        {
+            case 'f1':
+                logo.setAttribute('src', 'images/f1-logo160.png');
+                break;
+            case 'skyf1':
+                logo.setAttribute('src', 'images/skyf1-logo.jpg');
+                break;
+            case 'BBCF1':
+                logo.setAttribute('src', 'images/bbcf1-logo160.jpg');
+                break;
+            case 'WTF1':
+                logo.setAttribute('src', 'images/wtf1-logo160.png');
+                break;
+            case 'autosport':
+                logo.setAttribute('src', 'images/autosport-logo.png');
+                break;
+        }
+
+        card.appendChild(logo);
+        card.appendChild(title);
+
+        parent.appendChild(card);
+    });
 }
 
-setInterval(refresh_content, 1000*60);
+//setInterval(refresh_content, 1000*60);
